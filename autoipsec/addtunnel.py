370b8cde55d1fe0ea5b3d821442500f164843036
aman <amakhosa@cisco.com>
2020-12-09 09:59:56 -0800

    Fixed copying mistake

---
+++ autoipsec/addtunnel.py
@@ -0,0 +1,3 @@
+tunnel_name = 'vm3'
+tunnel_id = 'test-vm3' #Must be between 8 and 100 characters
+tunnel_pwd = 'snQLago4oHVez4QRJTG' #16-64 characters, 1 upper, lower, numeral. No special. Generate from 1password.
